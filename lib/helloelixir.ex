c6d16d7386e19956a3d9104c0696a38d5ddc886a
hammercui <hammercui@163.com>
2020-03-14 19:05:34 +0800

    init

---
+++ lib/helloelixir.ex
@@ -0,0 +1,18 @@
+defmodule Helloelixir do
+  @moduledoc """
+  Documentation for `Helloelixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Helloelixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
