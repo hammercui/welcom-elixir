0af4c8399eba95bce319e095eaf6ec989c05f28e
hammercui <hammercui@163.com>
2020-04-17 18:51:17 +0800

    update lesson

--- lib/testEnum.exs
+++ lib/testEnum.exs
@@ -1,1 +1,11 @@
 # 学习enum模块
+#Enum.__info__(:functions)
+#|> Enum.each(
+#     fn({function, arity}) ->
+#       IO.puts "#{function}/#{arity}"
+#     end)
+
+#all?遍历，函数所有返回true时，返回true
+ret = Enum.all?(["aaa","bbb"],fn(s)->String.length(s) == 3 end)
+IO.puts(ret)
+IO.puts(ret)
